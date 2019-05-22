e61af4fe0bc1f79fd7814705d5dd1c9179987bfb
TTimTT <tim.tuuva@epfl.ch>
2019-05-22 15:34:19 +0200

    Correction

--- code/Anael_test.m
+++ code/Anael_test.m
@@ -9,28 +9,32 @@ clc; clear all; close all;
 
 % Parameters
 P.ReceiverType  = 'Rake';
-P.NumberOfFrames      = 50;
-P.NumberOfSymbols     = 172; % TODO, in simulator: NumberOfBits   = P.NumberOfSymbols*P.Modulation*RX; % per Frame
+P.NumberOfFrames      = 500;
+P.NumberOfBits     = 172; % TODO, in simulator: NumberOfBits   = P.NumberOfSymbols*P.Modulation*RX; % per Frame
+P.Q_Ind = 12;
 
 P.AccessType = 'CDMA';
 P.CDMAUsers     = 1;
 
 P.Modulation    = 1;        % 1: BPSK, 2: 64ary
 
-P.ChannelType   = 'AWGN'; % 'Multipath', 'Fading'
+P.ChannelType   = 'AWGN'; % 'Multipath', 'Fading', 'AWGN', 'ByPass'
 P.ChannelLength = 1;
 
 P.HadLen = 64; % Length of Hadamard Sequence
 P.HadIn = 6; % Length of Hadamard input (6 -> 64)
 
 P.K = 9; % Length of convolutional encoder
-P.ConvSeq = [557 663 711]; % Rate 1/3
-P.Rate = 3;
+P.ConvSeq = [753 561]; % Rate 1/3
+P.Rate = length(P.ConvSeq);
 
 P.LongCodeLength = 42; % PN Sequence
 P.RakeFingers = 2; 
 
-P.SNRRange = -50:10:0; % SNR Range to simulate in dB
+P.SequenceMask = [1,1,0,0,0,1,1,0,0,0, randi([0 1],1,32)];
+
+
+P.SNRRange = -50:5:0; % SNR Range to simulate in dB
 
 % P.ReceiverType  = 'Rake'; % Only one type for the project
 
